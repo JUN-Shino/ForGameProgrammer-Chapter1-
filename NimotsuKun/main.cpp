52a7fbec306fa9a805fee5134e781dbabed6acc2
JUN <jun227w5@gmail.com>
2014-10-26 22:29:00 +0900

    ディレクトリ変更

---
+++ NimotsuKun/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//#壁 _空間 .ゴール oブロック p人
+const char gStageData[] = "\
+########\n\
+# .. p #\n\
+# oo   #\n\
+#      #\n\
+########";
+const int gStageWidth = 8;
+const int gStageHeight = 5;
+
+enum Object{
+	OBJ_SPACE,
+	OBJ_WALL,
+	OBJ_GOAL,
+	OBJ_BLOCK,
+	OBJ_BLOCK_ON_GOAL,
+	OBJ_MAN,
+	OBJ_MAN_ON_GOAL,
+
+	OBJ_UNKNOWN,
+};
+
+//関数プロトタイプ
+void initialize( Object* state, int w, int h, const char* stageData );
+void draw( const Object* state, int w, int h );
+void update( Object* state, char input, int w, int h );
+bool checkClear( const Object* state, int w, int h );
+
