5bfbe86da88efc8a2b5712b435eaf58c853bc687
JUN <jun227w5@gmail.com>
2014-11-04 00:27:59 +0900

    定義

--- NimotsuKun/main.cpp
+++ NimotsuKun/main.cpp
@@ -32,6 +32,54 @@ bool checkClear( const Object* state, int w, int h );
 
 int main(){
 	//状態配列確保
-	Object* state = new Object[gstageWidth * gStageHeight];
+	Object* state = new Object[gStageWidth * gStageHeight];
 	//ステージ初期化
+	initialize(state, gStageWidth, gStageHeight, gStageData);
+
+	//メインループ
+	while (true){
+		//まず描画
+		draw(state, gStageWidth, gStageHeight);
+		//クリアチェック
+		if (checkClear(state, gStageWidth, gStageHeight)){
+			break;//クリアチェック
+		}
+		//入力取得
+		cout << "a:left S:right w:up z:down. command?" << endl;//操作説明
+		char input;
+		cin >> input;
+		//更新
+		update(state, input, gStageWidth, gStageHeight);
+	}
+	//祝いのメッセージ
+	cout << "Congratulation's! you won." << endl;
+	//後始末
+	delete[] state;
+	state = 0;
+
+	//VS用
+	while (true){
+		;
+	}
+	return 0;
 }
+
+//---------------------以下関数定義--------------------
+
+void initialize(Object* state, int width, int/*height*/, const char* stageData){
+	const char * d = stageData; //読み込みポインタ
+	int x = 0;
+	int y = 0;
+	while (*d != '\0'){ //NULL文字でない間
+		Object t;
+		switch (*d){
+		case '#':t = OBJ_WALL; break;
+		case ' ':t = OBJ_SPACE; break;
+	
+
+	}
+}
+
+
+
+
